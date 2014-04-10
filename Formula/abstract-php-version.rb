7fa910647eb61eb7952fe5cbe59d6ab19fc23635
Shallker <imshallker@gmail.com>
2014-04-10 20:23:05 +0800

    Update abstract-php-version.rb

--- Formula/abstract-php-version.rb
+++ Formula/abstract-php-version.rb
@@ -44,7 +44,7 @@ class AbstractPhpVersion < Formula
   end
 
   module Php54Defs
-    PHP_SRC_TARBALL = 'http://www.php.net/get/php-5.4.16.tar.bz2/from/this/mirror'
+    PHP_SRC_TARBALL = 'http://museum.php.net/php5/php-5.4.16.tar.bz2'
     PHP_GITHUB_URL  = 'https://github.com/php/php-src.git'
     PHP_VERSION     = '5.4.27'
     PHP_BRANCH      = 'PHP-5.4'
